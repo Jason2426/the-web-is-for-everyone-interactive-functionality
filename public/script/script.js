8bf35cb69f1af64218c6e9dc97081a3726ad6bbe
Jason <jay.jpy.wong@gmail.com>
2024-03-25 10:35:31 +0100

    Hamburger menu fix, if ... element is available do this
    fix ty cyD

--- public/script/script.js
+++ public/script/script.js
@@ -1,27 +1,29 @@
-/* Clipboard API function */ 
+/* Clipboard API function */
+const copyButton = document.querySelector('.copy_button');
+if (copyButton) {
+    copyButton.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent default button behavior
 
-document.querySelector('.copy_button').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default button behavior
+        // Get the current URL
+        const fullLink = window.location.href;
 
-    // Get the current URL
-    var fullLink = window.location.href;
-
-    // Copy the URL to the clipboard
-    navigator.clipboard.writeText(fullLink)
-        .then(function() {
-            // Show success message
-            copySuccess.hidden = false;
-            copySuccess.style.display = 'flex';
-            copySuccess.style.alignItems = 'center';
-            // Hide copy button
-            event.target.hidden = true;
-        })
-        .catch(function(err) {
-            // Handle errors
-            console.error('Error copying link to clipboard: ', err);
-            alert('Link kopieren mislukt...');
-        });
-});
+        // Copy the URL to the clipboard
+        navigator.clipboard.writeText(fullLink)
+            .then(function () {
+                // Show success message
+                copySuccess.hidden = false;
+                copySuccess.style.display = 'flex';
+                copySuccess.style.alignItems = 'center';
+                // Hide copy button
+                event.target.hidden = true;
+            })
+            .catch(function (err) {
+                // Handle errors
+                console.error('Error copying link to clipboard: ', err);
+                alert('Link kopieren mislukt...');
+            });
+    });
+}
 
 
 
@@ -30,27 +32,31 @@ document.querySelector('.copy_button').addEventListener('click', function(event)
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    var menuIcon = document.querySelector('.menu_icon');
-    var burgerMenu = document.querySelector('.burger_menu_window');
-    var closeIcon = document.querySelector('.close_icon');
 
-    menuIcon.addEventListener('click', function () {
-        // Change translate position of menu icon
-        menuIcon.style.transform = 'translateX(-200%)';
-        // Show burger menu
-        burgerMenu.style.transform = 'translateX(0)';
-        // Hide menu icon
-        menuIcon.hidden = true;
-    });
+    const menuIcon = document.querySelector('.menu_icon');
+    if (menuIcon) {
+        const burgerMenu = document.querySelector('.burger_menu_window');
+        const closeIcon = document.querySelector('.close_icon');
 
-    closeIcon.addEventListener('click', function () {
-        // Change translate position of menu icon back to default
-        menuIcon.style.transform = 'translateX(0)';
-        // Hide burger menu
-        burgerMenu.style.transform = 'translateX(-200%)';
-        // Show menu icon
-        menuIcon.hidden = false;
-    });
+        menuIcon.addEventListener('click', function () {
+            // Change translate position of menu icon
+            menuIcon.style.transform = 'translateX(-200%)';
+            // Show burger menu
+            burgerMenu.style.transform = 'translateX(0)';
+            // Hide menu icon
+            menuIcon.hidden = true;
+        });
+
+        closeIcon.addEventListener('click', function () {
+            // Change translate position of menu icon back to default
+            menuIcon.style.transform = 'translateX(0)';
+            // Hide burger menu
+            burgerMenu.style.transform = 'translateX(-200%)';
+            // Show menu icon
+            menuIcon.hidden = false;
+        });
+
+    }
 });
 
 
